8f2fb33c643e8191e1d23a7cde8638f5c6acfe7b
Catalina Erlich <cerlich@si-b3.kzn.21-school.ru>
2021-06-28 17:59:52 +0300

    fin

--- cpp01/ex06/Karen.cpp
+++ cpp01/ex06/Karen.cpp
@@ -41,20 +41,23 @@ void Karen::complain(std::string level)
 
 void Karen::debug(void)
 {
-    std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
+    std::cout << "[DEBUG]\nI love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!\n" << std::endl;
+    info();
 }
 
 void Karen::info(void)
 {
-    std::cout <<  "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
+    std::cout <<  "[INFO]\nI cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!\n" << std::endl;
+    warning();
 }
 
 void Karen::warning(void)
 {
-     std::cout <<  "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
+     std::cout <<  "[WARNING]\nI think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month.\n" << std::endl;
+    error();
 }
 
 void Karen::error(void)
 {
-    std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
+    std::cout << "[ERROR]\nThis is unacceptable, I want to speak to the manager now." << std::endl;
 }
